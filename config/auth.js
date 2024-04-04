66b9c086a6dd958231af1cd1b122df2de2f36a4b
shyamQuantana <shyam.a@quantana.in>
2024-04-04 11:00:29 +0530

    changes for initial commit

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//checking for the commit
 const Constants = require('./constants');
 exports.authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
